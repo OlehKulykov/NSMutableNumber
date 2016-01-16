420b03d712bfb9f5cb6b460d86ab5a7ed53b8557
OlehKulykov <nonamedemail@gmail.com>
2016-01-16 16:40:28 +0100

    Release 1.0.14

---
+++ tests/MN/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MN
+//
+//  Created by Resident evil on 11/11/15.
+//  Copyright © 2015 Oleh Kulykov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
